f72317404f7c31fa9ed2cb054447e2bb86d48969
Ylenia <yleniamighela@gmail.com>
2025-03-11 17:44:46 +0100

    added components ReviewCard

---
+++ src/components/MovieReviewCard.jsx
@@ -0,0 +1,25 @@
+
+
+export default function MovieReviewCard() {
+
+
+    return (
+
+
+
+        <div>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam fuga dolores fugit maiores eum amet ullam itaque possimus sed necessitatibus deserunt blanditiis optio rerum, rem qui porro vitae beatae.</p>
+
+            <strong> Vote: 5</strong>
+
+            <address> da Andrea</address>
+
+
+        </div>
+
+
+
+
+    );
+}
--- src/pages/DetailPageMovie.jsx
+++ src/pages/DetailPageMovie.jsx
@@ -1,15 +1,32 @@
+import { Link } from "react-router-dom"
+// importo il componente moviecard
+import MovieReviewCard from "../components/MovieReviewCard"
+
 export default function DetailPageMovie() {
 
 
     return (
 
         <div>
+
+            <div>
+                <img src="http://localhost:3000/img/movies_cover/titanic.jpg" alt="" />
+            </div>
+
             <h2> Titolo FIlm</h2>
             <span> Nome autore</span>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, aspernatur?
                 Ducimus possimus iste aliquam repellendus fugiat praesentium, velit deserunt
                 veniam modi placeat doloribus, cumque harum odit, nam ullam quos? Aperiam.</p>
 
+            <section>
+                <h5>Recensioni</h5>
+                <MovieReviewCard />
+                <MovieReviewCard />
+                <MovieReviewCard />
+            </section>
+
+            <Link to="/">Torna alla Home</Link>
         </div>
 
 
