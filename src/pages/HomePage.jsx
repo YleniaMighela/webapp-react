16ce2f571957fe95df36c4ece34e747d36f745ff
Ylenia <yleniamighela@gmail.com>
2025-03-12 14:52:38 +0100

    added useState and useEffect for axios Movie

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,33 @@
+// importo axios per effetuare le chiamate
+import axios from 'axios'
+
+// importo il componente
+import { useState, useEffect } from "react";
 import MovieListCard from "../components/MovieListCard"
 
 export default function HomePage() {
+
+    // setto la variabile si stato
+    const [movies, setMovies] = useState([]);
+
+    //funzione di gestione chiamata API per i FILM
+    function fetchMovies() {
+
+        axios.get("http://localhost:3000/api/movies")
+
+            .then((res) => {
+                setMovies(res.data);
+                // console.log(res.data);
+
+
+            })
+
+            .catch(err => console.log(err));
+
+    }
+
+    useEffect(fetchMovies, [])
+
 
 
     return (
