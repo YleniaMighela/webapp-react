31a461d01a05e3de6875b7a4f88f106b0738ca84
Ylenia <yleniamighela@gmail.com>
2025-03-15 10:41:24 +0100

    added axios and logic

--- src/pages/CreateNewMoviePage.jsx
+++ src/pages/CreateNewMoviePage.jsx
@@ -1,78 +1,132 @@
+import axios from "axios";
+
+// importo parte LInk del modulo react-router
+import { useNavigate } from "react-router-dom"
+
+// importiamo lo useState 
+import { useState } from "react";
+
+const initialData = {
+
+    title: "",
+    director: "",
+    genre: "",
+    image: null,
+    abstract: ""
+};
+
+const endpointApi = "http://localhost:3000/api/movies";
+
 export default function CreateNewMoviePage() {
+
+
+    // navigazione
+    const navigate = useNavigate();
+
+    // states iniziali
+    const [formDataObj, setFormDataObj] = useState(initialData);
+
+    const setFieldValue = (e) => {
+        const { value, name } = e.target;
+        if (name === "image") setFormDataObj({ ...formDataObj, image: e.target.files[0] });
+        else setFormDataObj({ ...formDataObj, [name]: value });
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        axios.post(endpointApi, formDataObj, { headers: { "Content-Type": "multipart/form-data" } })
+            .then(
+                () => { navigate("/") }
+            )
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+
 
 
 
     return (
+        <>
 
+            <section>
+                <form onSubmit={handleSubmit} >
+                    <div>
 
-        <section>
-            <form >
-                <div>
+                        <label>Titolo:</label>
+                        <input
 
-                    <label>Titolo:</label>
-                    <input
+                            name="title"
+                            type="text"
+                            placeholder='Inserisci titolo'
+                            value={formDataObj.title}
+                            onChange={setFieldValue}
 
-                        name="title"
-                        type="text"
-                        placeholder='Inserisci titolo'
+                        />
+                    </div>
 
-                    />
-                </div>
+                    <div >
 
-                <div >
+                        <label>Regista:</label>
+                        <input
 
-                    <label>Regista:</label>
-                    <input
+                            name="director"
+                            type="text"
+                            placeholder='Inserisci il regista'
+                            value={formDataObj.director}
+                            onChange={setFieldValue}
 
-                        name="director"
-                        type="text"
-                        placeholder='Inserisci il regista'
+                        />
+                    </div>
 
-                    />
-                </div>
+                    <div >
 
-                <div >
+                        <label>Genere:</label>
+                        <input
 
-                    <label>Genere:</label>
-                    <input
+                            name="genre"
+                            type="text"
+                            placeholder='Inserisci un genere'
+                            value={formDataObj.genre}
+                            onChange={setFieldValue}
 
-                        name="genre"
-                        type="text"
-                        placeholder='Inserisci un genere'
-
-                    />
-                </div>
-
-
-
-                <div >
-
-                    <label>Immagine:</label>
-                    <input
-                        name="image"
-                        type="file"
-
-                    />
-                </div>
-
-                <div >
-
-                    <label>Estratto:</label>
-                    <textarea
-                        name="abstract"
-                        placeholder='Inserisci un estratto'
-                    ></textarea>
-                </div>
-
-                <div >
-                    <button type="submit">
-                        Aggiungi film
-                    </button>
-                </div>
-            </form>
-        </section>
+                        />
+                    </div>
 
 
 
+                    <div >
+
+                        <label>Immagine:</label>
+                        <input
+                            name="image"
+                            type="file"
+                            onChange={setFieldValue}
+                        />
+                    </div>
+
+                    <div >
+
+                        <label>Estratto:</label>
+                        <textarea
+                            name="abstract"
+                            placeholder='Inserisci un estratto'
+                            onChange={setFieldValue}
+                        ></textarea>
+                    </div>
+
+                    <div >
+                        <button type="submit">
+                            Aggiungi film
+                        </button>
+                    </div>
+                </form>
+            </section>
+
+
+        </>
     );
 }
